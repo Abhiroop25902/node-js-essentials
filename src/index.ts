6714dd6d2d8c9009e69d91153ab0607805ea3ebd
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-08-04 20:01:58 +0530

    initial commit before await problem fix

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as fsBase from "node:fs";
+
+const fs = fsBase.promises;
+
+// const data = await fs.readFile(path.join(__dirname, "data.json"), "utf8");
+//
+// console.log(data);
+
+console.log("Hello");
